869bf0eb770eb934ade20c462ef7724343a1eeeb
Mansur Kodzoev <71566968+DonGarp@users.noreply.github.com>
2021-09-14 18:41:44 +0300

    Добавьте файлы проекта.

---
+++ ArtsecHardwareUpdater/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿
+namespace ArtsecHardwareUpdater
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxSettings = new System.Windows.Forms.GroupBox();
+            this.textBoxDelay = new System.Windows.Forms.TextBox();
+            this.labelDelay = new System.Windows.Forms.Label();
+            this.labelPort = new System.Windows.Forms.Label();
+            this.labelIP = new System.Windows.Forms.Label();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBoxCommonInfo = new System.Windows.Forms.GroupBox();
+            this.buttonChooseFile = new System.Windows.Forms.Button();
+            this.textBoxFilePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBoxLog = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonSaveLogs = new System.Windows.Forms.Button();
+            this.buttonClearLogs = new System.Windows.Forms.Button();
+            this.groupBoxSettings.SuspendLayout();
+            this.groupBoxCommonInfo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxSettings
+            // 
+            this.groupBoxSettings.Controls.Add(this.textBoxDelay);
+            this.groupBoxSettings.Controls.Add(this.labelDelay);
+            this.groupBoxSettings.Controls.Add(this.labelPort);
+            this.groupBoxSettings.Controls.Add(this.labelIP);
+            this.groupBoxSettings.Controls.Add(this.textBoxPort);
+            this.groupBoxSettings.Controls.Add(this.textBoxIP);
+            this.groupBoxSettings.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxSettings.Name = "groupBoxSettings";
+            this.groupBoxSettings.Size = new System.Drawing.Size(760, 120);
+            this.groupBoxSettings.TabIndex = 0;
+            this.groupBoxSettings.TabStop = false;
+            this.groupBoxSettings.Text = "Настройки";
+            // 
+            // textBoxDelay
+            // 
+            this.textBoxDelay.Location = new System.Drawing.Point(270, 20);
+            this.textBoxDelay.Name = "textBoxDelay";
+            this.textBoxDelay.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDelay.TabIndex = 5;
+            this.textBoxDelay.Text = "2000";
+            // 
+            // labelDelay
+            // 
+            this.labelDelay.AutoSize = true;
+            this.labelDelay.Location = new System.Drawing.Point(180, 23);
+            this.labelDelay.Name = "labelDelay";
+            this.labelDelay.Size = new System.Drawing.Size(87, 13);
+            this.labelDelay.TabIndex = 4;
+            this.labelDelay.Text = "Задержка (мс): ";
+            // 
+            // labelPort
+            // 
+            this.labelPort.AutoSize = true;
+            this.labelPort.Location = new System.Drawing.Point(14, 48);
+            this.labelPort.Name = "labelPort";
+            this.labelPort.Size = new System.Drawing.Size(35, 13);
+            this.labelPort.TabIndex = 3;
+            this.labelPort.Text = "Порт:";
+            // 
+            // labelIP
+            // 
+            this.labelIP.AutoSize = true;
+            this.labelIP.Location = new System.Drawing.Point(14, 23);
+            this.labelIP.Name = "labelIP";
+            this.labelIP.Size = new System.Drawing.Size(53, 13);
+            this.labelIP.TabIndex = 2;
+            this.labelIP.Text = "IP-адрес:";
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(75, 45);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPort.TabIndex = 1;
+            this.textBoxPort.Text = "69";
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(75, 20);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(100, 20);
+            this.textBoxIP.TabIndex = 0;
+            this.textBoxIP.Text = "192.168.1.128";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(697, 464);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Начать";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 428);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(760, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // groupBoxCommonInfo
+            // 
+            this.groupBoxCommonInfo.Controls.Add(this.buttonChooseFile);
+            this.groupBoxCommonInfo.Controls.Add(this.textBoxFilePath);
+            this.groupBoxCommonInfo.Controls.Add(this.label1);
+            this.groupBoxCommonInfo.Location = new System.Drawing.Point(12, 138);
+            this.groupBoxCommonInfo.Name = "groupBoxCommonInfo";
+            this.groupBoxCommonInfo.Size = new System.Drawing.Size(760, 120);
+            this.groupBoxCommonInfo.TabIndex = 3;
+            this.groupBoxCommonInfo.TabStop = false;
+            this.groupBoxCommonInfo.Text = "Общие сведения";
+            // 
+            // buttonChooseFile
+            // 
+            this.buttonChooseFile.Location = new System.Drawing.Point(679, 23);
+            this.buttonChooseFile.Name = "buttonChooseFile";
+            this.buttonChooseFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonChooseFile.TabIndex = 2;
+            this.buttonChooseFile.Text = "Обзор";
+            this.buttonChooseFile.UseVisualStyleBackColor = true;
+            this.buttonChooseFile.Click += new System.EventHandler(this.buttonChooseFile_Click);
+            // 
+            // textBoxFilePath
+            // 
+            this.textBoxFilePath.Location = new System.Drawing.Point(89, 25);
+            this.textBoxFilePath.Name = "textBoxFilePath";
+            this.textBoxFilePath.ReadOnly = true;
+            this.textBoxFilePath.Size = new System.Drawing.Size(584, 20);
+            this.textBoxFilePath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Путь к файлу:";
+            // 
+            // richTextBoxLog
+            // 
+            this.richTextBoxLog.Location = new System.Drawing.Point(12, 288);
+            this.richTextBoxLog.Name = "richTextBoxLog";
+            this.richTextBoxLog.Size = new System.Drawing.Size(760, 96);
+            this.richTextBoxLog.TabIndex = 4;
+            this.richTextBoxLog.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 265);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Логи:";
+            // 
+            // buttonSaveLogs
+            // 
+            this.buttonSaveLogs.Location = new System.Drawing.Point(697, 390);
+            this.buttonSaveLogs.Name = "buttonSaveLogs";
+            this.buttonSaveLogs.Size = new System.Drawing.Size(75, 23);
+            this.buttonSaveLogs.TabIndex = 6;
+            this.buttonSaveLogs.Text = "Сохранить";
+            this.buttonSaveLogs.UseVisualStyleBackColor = true;
+            // 
+            // buttonClearLogs
+            // 
+            this.buttonClearLogs.Location = new System.Drawing.Point(616, 390);
+            this.buttonClearLogs.Name = "buttonClearLogs";
+            this.buttonClearLogs.Size = new System.Drawing.Size(75, 23);
+            this.buttonClearLogs.TabIndex = 7;
+            this.buttonClearLogs.Text = "Очистить";
+            this.buttonClearLogs.UseVisualStyleBackColor = true;
+            this.buttonClearLogs.Click += new System.EventHandler(this.buttonClearLogs_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 496);
+            this.Controls.Add(this.buttonClearLogs);
+            this.Controls.Add(this.buttonSaveLogs);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBoxLog);
+            this.Controls.Add(this.groupBoxCommonInfo);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.groupBoxSettings);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Artsec Hardware Updater";
+            this.groupBoxSettings.ResumeLayout(false);
+            this.groupBoxSettings.PerformLayout();
+            this.groupBoxCommonInfo.ResumeLayout(false);
+            this.groupBoxCommonInfo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxSettings;
+        private System.Windows.Forms.TextBox textBoxDelay;
+        private System.Windows.Forms.Label labelDelay;
+        private System.Windows.Forms.Label labelPort;
+        private System.Windows.Forms.Label labelIP;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox groupBoxCommonInfo;
+        private System.Windows.Forms.Button buttonChooseFile;
+        private System.Windows.Forms.TextBox textBoxFilePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBoxLog;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonSaveLogs;
+        private System.Windows.Forms.Button buttonClearLogs;
+    }
+}
+
---
+++ ArtsecHardwareUpdater/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Threading;
+using FluentFTP;
+using System.Net.Sockets;
+
+namespace ArtsecHardwareUpdater
+{
+    public partial class FormMain : Form
+    {
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        const string helpstring = "SomeUdp <mode> [file] [address]\n" +
+           "\tmode:\n" +
+           "\t\t-s - sender mode\n" +
+           "\t\t-r - receiver mode\n" +
+           "\tfile - file to send\n" +
+           "\taddress - host to send\n";
+        const int sendPort = 20600; //Номер порта, который будет открываться для отправки пакетов
+        const int receivePort = 20601; //Номер порта, который будет получать пакеты
+        const int packetSize = 8192; //Макс размер пакета
+
+        static UdpClient udpSender; //Экземпляр класса, для работы с Udp для отправки пакетов
+        static UdpClient udpReceiver;//Экземпляр класса, для работы с Udp для получения пакетов
+
+        /// <summary>
+        /// Функция, для отправки пакетов
+        /// </summary>
+        /// <param name="path">путь файла</param>
+        /// <param name="address">хост-получатель</param>
+        static void Sender(string path, string address)
+        {
+            Console.WriteLine("Sender mode");
+            IPAddress ipAddr;
+            try
+            {
+                IPHostEntry entry = Dns.GetHostEntry(address); // Резловим введенный пользователем адрес
+                ipAddr = entry.AddressList.FirstOrDefault(a => a.AddressFamily == AddressFamily.InterNetwork); //получаем введеный пользователем адрес и проверяем адрес IpV4 или IpV6
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+            IPEndPoint sendEndPoint = new IPEndPoint(ipAddr, sendPort); //Конечная точка получателя
+            IPEndPoint receiveEndPoint = null; //Конечная точка Устройства, который ответит на принятый пакет
+            try
+            {
+                udpReceiver = new UdpClient(receivePort); //Экземпляр класса, для получение файла с заданного порта
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+            using (FileStream fsSource = new FileStream(path, FileMode.Open, FileAccess.Read)) //Работа с файлом
+            {
+                int numBytesToRead = (int)fsSource.Length; //Количество байтов на чтение
+                int numBytesReaded = 0;//Количество байтов прочитано получателем
+                string name = Path.GetFileName(path); //Получение имени файла, чтобы получатель знал какой файл с каким названием создать
+                byte[] packetSend; //пакеты для отправки
+                byte[] packetRecieve;//пакеты для приема
+                packetSend = Encoding.Unicode.GetBytes(name); //Подготавливаем к отрпавке пакет с названием файла
+                udpReceiver.Client.ReceiveTimeout = 5000; //время ожидания ответа
+                udpSender.Send(packetSend, packetSend.Length, sendEndPoint); // отправка название файла получателю
+                packetRecieve = udpReceiver.Receive(ref receiveEndPoint); //ожидаем ответа и записываем полученный ответ в переменную
+
+                int parts = (int)fsSource.Length / packetSize; //количество пакетов
+                if ((int)fsSource.Length % packetSize != 0)
+                {
+                    parts++;
+                }
+                packetSend = BitConverter.GetBytes(parts);
+                udpSender.Send(packetSend, packetSend.Length, sendEndPoint); //оповещаем получателя о количестве входящих пакетов файла
+                packetRecieve = udpReceiver.Receive(ref receiveEndPoint);
+
+                int n = 0;
+                packetSend = new byte[packetSize];
+                for (int i = 0; i < parts - 1; i++)
+                {
+                    n = fsSource.Read(packetSend, 0, packetSize);
+                    if (n == 0)
+                    {
+                        Console.WriteLine("Нет пакетов!");
+                        break;
+                    }
+                    numBytesReaded += n;
+                    numBytesToRead -= n;
+                    udpSender.Send(packetSend, packetSend.Length, sendEndPoint); //отправка файла
+                    packetRecieve = udpReceiver.Receive(ref receiveEndPoint);
+                }
+                packetSend = new byte[numBytesToRead];
+                n = fsSource.Read(packetSend, 0, numBytesToRead);
+                udpSender.Send(packetSend, packetSend.Length, sendEndPoint); //отправка последнего пакета файла
+                packetRecieve = udpReceiver.Receive(ref receiveEndPoint);
+            }
+            Console.WriteLine("file sent successfully");
+        }
+
+        static void Receiver()
+        {
+            Console.WriteLine("Receiver mode");
+            IPEndPoint receiveEndPoint = null;
+            try
+            {
+                udpReceiver = new UdpClient(sendPort);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+            byte[] packetSend = new byte[1];
+            byte[] packetReceive;
+            packetSend[0] = 1;
+            packetReceive = udpReceiver.Receive(ref receiveEndPoint);
+            IPEndPoint sendEndPoint = new IPEndPoint(receiveEndPoint.Address, receivePort);
+            string name = Encoding.Unicode.GetString(packetReceive);
+            udpSender.Send(packetSend, packetSend.Length, sendEndPoint);
+            udpReceiver.Client.ReceiveTimeout = 5000;
+            packetReceive = udpReceiver.Receive(ref receiveEndPoint);
+            int parts = BitConverter.ToInt32(packetReceive, 0);
+            using (FileStream fsDest = new FileStream(name, FileMode.Create, FileAccess.Write))
+            {
+                for (int i = 0; i < parts; i++)
+                {
+                    packetReceive = udpReceiver.Receive(ref receiveEndPoint);
+                    fsDest.Write(packetReceive, 0, packetReceive.Length);
+                    udpSender.Send(packetSend, packetSend.Length, sendEndPoint);
+                }
+            }
+            Console.WriteLine("File recieved");
+        }
+
+        async private void buttonStart_Click(object sender, EventArgs e)
+        {
+            string ip_address = textBoxIP.Text;
+            int port = int.Parse(textBoxPort.Text);
+            int delay = int.Parse(textBoxDelay.Text);
+            string file_path = textBoxFilePath.Text;
+            
+            richTextBoxLog.Text += "Начата загрузка файла!";
+            UdpClient udpSender = new UdpClient();
+
+            IPAddress ipAddr;
+            try
+            {
+                /*                ipAddr = IPAddress.Parse(ip_address);
+                                IPEndPoint point = new IPEndPoint(ipAddr, port);
+
+                                byte[] setBaudrate_cmd = { 0x46, 0x01, 0x02, 0xD2, 0xE5, 0xF1, 0xF2 };
+                                udpSender.Send(setBaudrate_cmd, setBaudrate_cmd.Length, point);*/
+
+                /*IPHostEntry entry = Dns.GetHostEntry(ip_address);
+                ipAddr = entry.AddressList.FirstOrDefault(a => a.AddressFamily == AddressFamily.InterNetwork);*/
+                ipAddr = IPAddress.Parse(ip_address);
+                IPEndPoint sendEndPoint = new IPEndPoint(ipAddr, port);
+                int n = 0;
+                
+/*                using (FileStream fs = new FileStream(file_path, FileMode.Open, FileAccess.Read))
+                {*/
+                    byte[] packetSend = { 0x00, 0x02, 0x73, 0x6c, 0x6f, 0x77, 0x5f, 0x62, 0x6c, 0x69, 0x6e, 0x6b, 0x2e, 0x62, 0x69, 0x6e, 0x00, 0x6f, 0x63, 0x74, 0x65, 0x74, 0x00 };
+                    /*n = fs.Read(packetSend,0, packetSend.Length);*/
+                    udpSender.Send(packetSend, packetSend.Length, sendEndPoint);
+
+
+                /*}*/
+            }
+            catch (Exception ex)
+            {
+                richTextBoxLog.ForeColor = Color.Red;
+                richTextBoxLog.Text += ex.Message;
+            }
+        }
+        
+
+        private void buttonChooseFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            /*openFileDialog.Filter = "Firmware File(*.bin, *.hex)|*.bin,*.hex";*/
+            openFileDialog.Filter = "Firmware File(*.bin)|*.bin|Firmware File(*.hex)|*.hex";
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    textBoxFilePath.Text = openFileDialog.FileName;           
+                }
+                catch
+                {
+                    DialogResult result = MessageBox.Show("Невозможно выбрать указанный файл!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void buttonClearLogs_Click(object sender, EventArgs e)
+        {
+            richTextBoxLog.Clear();
+        }
+    }
+}
+
+
+// ПЕРВЫЙ НЕУДАЧНЫЙ СПОСОБ ПОДКЛЮЧЕНИЯ
+//try
+//{
+//    using (var connection = new FtpClient())
+//    {
+//        connection.Host = ip_address;
+//        richTextBoxLog.Text += "Устанавливается соединение...\n";
+//        connection.ConnectTimeout = 1000;
+//        await connection.ConnectAsync();
+//        if (connection.IsConnected)
+//        {
+//            richTextBoxLog.ForeColor = Color.Green;
+//            richTextBoxLog.Text += "Соединение успешно установлено!\n";
+//            return;
+//        }
+//        else
+//        {
+//            richTextBoxLog.ForeColor = Color.Red;
+//            richTextBoxLog.Text += "Ошибка подключения! Повторное соединение...\n";
+//            await Task.Delay(delay);
+//        }
+//    }
+
+//}
+//catch (Exception ex)
+//{
+//    richTextBoxLog.Text += ex.Message;
+//    richTextBoxLog.Text += "sdfsfdsf";
+//}
+// ПЕРВЫЙ НЕУДАЧНЫЙ СПОСОБ ПОДКЛЮЧЕНИЯ
+//try
+//{
+//    using (var connection = new FtpClient())
+//    {
+//        connection.Host = ip_address;
+//        richTextBoxLog.Text += "Устанавливается соединение...\n";
+//        connection.ConnectTimeout = 1000;
+//        await connection.ConnectAsync();
+//        if (connection.IsConnected)
+//        {
+//            richTextBoxLog.ForeColor = Color.Green;
+//            richTextBoxLog.Text += "Соединение успешно установлено!\n";
+//            return;
+//        }
+//        else
+//        {
+//            richTextBoxLog.ForeColor = Color.Red;
+//            richTextBoxLog.Text += "Ошибка подключения! Повторное соединение...\n";
+//            await Task.Delay(delay);
+//        }
+//    }
+
+//}
+//catch (Exception ex)
+//{
+//    richTextBoxLog.Text += ex.Message;
+//    richTextBoxLog.Text += "sdfsfdsf";
+//}
